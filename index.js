a6546015d39d1ca894f274f919046aa230c37c17
nova1751 <nova2319427728@outlook.com>
2023-11-14 19:19:14 +0800

    feat: original version update

--- index.js
+++ index.js
@@ -1,24 +1,59 @@
-const rBacktick =
-  /^(?<start>\s*)(?<tick>~{3,}|`{3,})(?<args>.*)\n(?<code>[\s\S]*?)^\s*\k<tick>(?<end>\s*)$/gm;
 const shiki = require("shiki");
 const stripIndent = require("strip-indent");
+const themes = require("./lib/themes");
+const codeMatch =
+  /^(?<start>\s*)(?<tick>~{3,}|`{3,})\s*(?<args>.*)\n(?<code>[\s\S]*?)^\s*\k<tick>(?<end>\s*)$/gm;
+const theme = themes.has(config.theme) ? config.theme : "one-dark-pro";
 const config = hexo.config.shiki;
 const css = hexo.extend.helper.get("css").bind(hexo);
 const js = hexo.extend.helper.get("js").bind(hexo);
 hexo.extend.injector.register("head_end", () => {
-  return css("http://127.0.0.1:5501/lib/codeblock.css");
+  return css("https://unpkg.com/hexo-shiki-plugin@latest/lib/codeblock.css");
 });
-
+hexo.extend.injector.register("head_end", () => {
+  return themes.get(theme);
+});
+hexo.extend.injector.register("head_end", () => {
+  return `
+  <style>.code-expand-btn:not(.expand-done)~pre,.code-expand-btn:not(.expand-done)~table{height:${hexo.config.shiki.highlightHeightLimit}px}</style>
+  `;
+});
 hexo.extend.injector.register("body_end", () => {
-  return js("htt1://127.0.0.1:5501/lib/codeblock.js");
+  return js("https://unpkg.com/hexo-shiki-plugin@latest/lib/codeblock.js");
+});
+hexo.extend.injector.register("body_end", () => {
+  const {
+    beatutify,
+    highlight_copy,
+    highlight_lang,
+    highlight_height_limit,
+    is_highlight_shrink,
+    copy: { success, error, no_support },
+  } = hexo.config.shiki;
+  return `
+  <script>
+  const CODE_CONFIG = {
+    beautify: ${beatutify},
+    highlightCopy: ${highlight_copy},
+    highlightLang: ${highlight_lang},
+    highlightHeightLimit: ${highlight_height_limit},
+    isHighlightShrink: ${is_highlight_shrink},
+    copy: {
+      success: ${success},
+      error: ${error},
+      noSupport: ${no_support},
+    }
+  }
+  </script>
+  `;
 });
 return shiki
   .getHighlighter({
-    theme: config.theme,
+    theme,
   })
   .then((hl) => {
     hexo.extend.filter.register("before_post_render", (post) => {
-      post.content = post.content.replace(rBacktick, (...argv) => {
+      post.content = post.content.replace(codeMatch, (...argv) => {
         let { start, end, args, code } = argv.pop();
         let result;
         try {
@@ -32,18 +67,14 @@ return shiki
           result = `<figure class="shiki${args ? ` ${args}` : ""}"${
             args ? ` data-language="${args}"` : ""
           }>`;
-
           result += "<table><tr>";
-
           result += `<td class="gutter"><pre>${numbers}</pre></td>`;
-
           result += `<td class="code">${pre}</td>`;
           result += "</tr></table></figure>";
-
           return `${start}<hexoPostRenderCodeBlock>${result}</hexoPostRenderCodeBlock>${end}`;
         } catch (e) {
           console.error(e);
-          return result;
+          return `${start}<hexoPostRenderCodeBlock>${code}</hexoPostRenderCodeBlock>${end}`;
         }
       });
     });
