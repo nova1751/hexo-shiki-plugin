e47c91f163e17877dbeb0db6fad3db4a8b7e7207
nova1751 <nova2319427728@outlook.com>
2023-11-14 22:37:52 +0800

    fix: resolve the theme conflict

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Changelog
+
+## [1.0.8] - 2023-11-14
+
+### Bug fixes
+
+Remove the css class conflicts with theme butterfly
--- index.js
+++ index.js
@@ -17,7 +17,7 @@ hexo.extend.injector.register("head_end", () => {
 if (config.highlight_height_limit) {
   hexo.extend.injector.register("head_end", () => {
     return `
-  <style>.code-expand-btn:not(.expand-done)~pre,.code-expand-btn:not(.expand-done)~table{height:${config.highlight_height_limit}px}</style>
+    <style>.code-expand-btn:not(.expand-done)~table,.code-expand-btn:not(.expand-done)~*table{overflow:hidden;height:${config.highlight_height_limit}px</style>
   `;
   });
 }
